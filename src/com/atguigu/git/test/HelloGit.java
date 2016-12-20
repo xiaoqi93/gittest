5c5875ca25a26cb580365f8afc662e647c4da7fe
xiaoqi93 <954297967@qq.com>
2016-12-20 18:53:36 +0800

    update

--- src/com/atguigu/git/test/HelloGit.java
+++ src/com/atguigu/git/test/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello github ,第一次提交");
 		System.out.println("hello github ,第二次提交");
+                System.out.println("hello github ,收到第一次请求");
 	}
 }
