de0c384e1c410305bd2985d3d316adfa1666175b
xiaoqi <954297967@qq.com>
2016-12-20 19:26:47 +0800

    第三次

--- src/com/atguigu/git/test/HelloGit.java
+++ src/com/atguigu/git/test/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello github ,第一次提交");
 		System.out.println("hello github ,第二次提交");
-                System.out.println("hello github ,收到第一次请求");
+		//哈哈哈
+        System.out.println("hello github ,收到第一次请求");
 	}
 }
